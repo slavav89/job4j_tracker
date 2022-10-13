7efc841c0ee1f2971285044b232d00e9371b12f3
Вячеслав <slava_zen@mail.ru>
2022-10-13 19:25:07 +0500

    ru.job4j.oop.Error

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,37 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+        this.active = true;
+        this.status = 1;
+        this.message = "error: ';' expected";
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Активность: " + active);
+        System.out.println("Статус: " + status + " error");
+        System.out.println("Сообщение: " + message + "\n");
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.printInfo();
+        Error error1 = new Error(false, 0, "Ошибок нет");
+        error1.printInfo();
+        Error error2 = new Error(true, 1, "error: reached end of file while parsing");
+        error2.printInfo();
+        Error error3 = new Error(true, 1, "error: illegal start of expression");
+        error3.printInfo();
+    }
+}
