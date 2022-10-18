e3074117a19e79423c1a1730d0fa866af83532f4
Вячеслав <slava_zen@mail.ru>
2022-10-18 17:36:32 +0500

    ru.job4j.oop.Reduce

---
+++ src/main/java/ru/job4j/oop/Reduce.java
@@ -0,0 +1,26 @@
+package ru.job4j.oop;
+
+public class Reduce {
+    private int[] array;
+
+    /**
+     * Была ошибка связанная с однинаковыми названиями
+     * переменных у поля класса и аргумента метода to()
+     */
+    public void to(int[] arrays) {
+        array = arrays;
+    }
+
+    public void print() {
+        for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3};
+        Reduce reduce = new Reduce();
+        reduce.to(array);
+        reduce.print();
+    }
+}
