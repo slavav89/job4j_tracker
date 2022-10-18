c4d0d1e985ca9d5ca77e357079828e527061f7c3
Вячеслав <slava_zen@mail.ru>
2022-10-18 16:11:43 +0500

    ru.job4j.oop.Point

--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -1,7 +1,9 @@
 package ru.job4j.oop;
 
-import org.assertj.core.data.Percentage;
+import static org.assertj.core.api.Assertions.offset;
+
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PointTest {
@@ -12,7 +14,7 @@ public class PointTest {
         Point b = new Point(1, 2);
         double dist = a.distance(b);
         double expected = 6.403;
-        assertThat(dist).isCloseTo(expected, Percentage.withPercentage(0.01));
+        assertThat(dist).isCloseTo(expected, offset(0.001));
     }
 
     @Test
@@ -21,7 +23,7 @@ public class PointTest {
         Point b = new Point(0, 2);
         double dist = a.distance(b);
         double expected = 2.0;
-        assertThat(dist).isCloseTo(expected, Percentage.withPercentage(0.01));
+        assertThat(dist).isCloseTo(expected, offset(0.001));
     }
 
     @Test
@@ -30,7 +32,7 @@ public class PointTest {
         Point b = new Point(0, 4);
         double dist = a.distance(b);
         double expected = 2.0;
-        assertThat(dist).isCloseTo(expected, Percentage.withPercentage(0.01));
+        assertThat(dist).isCloseTo(expected, offset(0.001));
     }
 
     @Test
@@ -39,6 +41,24 @@ public class PointTest {
         Point b = new Point(90, 3);
         double dist = a.distance(b);
         double expected = 80.006;
-        assertThat(dist).isCloseTo(expected, Percentage.withPercentage(0.01));
+        assertThat(dist).isCloseTo(expected, offset(0.001));
+    }
+
+    @Test
+    public void whenThis789That123Then10dot392() {
+        Point a = new Point(7, 8, 9);
+        Point b = new Point(1, 2, 3);
+        double dist = a.distance3d(b);
+        double expected = 10.392;
+        assertThat(dist).isCloseTo(expected, offset(0.001));
+    }
+
+    @Test
+    public void whenThis073That294Then3dot0() {
+        Point a = new Point(0, 7, 3);
+        Point b = new Point(2, 9, 4);
+        double dist = a.distance3d(b);
+        double expected = 3.0;
+        assertThat(dist).isCloseTo(expected, offset(0.001));
     }
 }
