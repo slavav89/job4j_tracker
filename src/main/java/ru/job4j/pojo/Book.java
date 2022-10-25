b6309e6f692775a3b2b68b0314f5b55376c55d81
Вячеслав <slava_zen@mail.ru>
2022-10-25 17:14:42 +0500

    ru.job4j.pojo.Library

---
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -0,0 +1,30 @@
+package ru.job4j.pojo;
+
+public class Book {
+    private String name;
+    private int page;
+
+    public Book(String name, int page) {
+        this.name = name;
+        this.page = page;
+    }
+
+    public Book() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,38 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book cleanCode = new Book("Clean code", 50);
+        Book alchemist = new Book("Alchemist", 100);
+        Book girls = new Book("Girls", 150);
+        Book cinderella = new Book("cinderella", 200);
+        Book temp = new Book();
+        Book[] books = new Book[4];
+        books[0] = cleanCode;
+        books[1] = alchemist;
+        books[2] = girls;
+        books[3] = cinderella;
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            System.out.println("Название книги " + book.getName()
+                    + " c количеством страниц: " + book.getPage());
+        }
+        System.out.println("\nНовый порядок книг\n");
+        temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            System.out.println("Название книги " + book.getName()
+                    + " c количеством страниц: " + book.getPage());
+        }
+        System.out.println("\nвывод книги с именем Clean code\n");
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            if ("Clean code".equals(book.getName())) {
+                System.out.println("Название книги " + book.getName()
+                        + " c количеством страниц: " + book.getPage());
+            }
+        }
+    }
+}
