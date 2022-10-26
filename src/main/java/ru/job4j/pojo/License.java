9c0b08329f681ff512bb50c5da6b91e2a822f347
Вячеслав <slava_zen@mail.ru>
2022-10-26 21:58:42 +0500

    ru.job4j.pojo.License

--- src/main/java/ru/job4j/pojo/License.java
+++ src/main/java/ru/job4j/pojo/License.java
@@ -9,13 +9,19 @@ public class License {
     private String code;
     private Date created;
 
+    public License() {
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         License license = (License) o;
-        return Objects.equals(owner, license.owner) && Objects.equals(model, license.model)
-                && Objects.equals(code, license.code) && Objects.equals(created, license.created);
+        return Objects.equals(owner, license.owner) && Objects.equals(model, license.model) && Objects.equals(code, license.code) && Objects.equals(created, license.created);
     }
 
     @Override
