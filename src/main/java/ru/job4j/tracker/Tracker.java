329509d176e9aebd9ad26eb055664bde5e21b880
Вячеслав <slava_zen@mail.ru>
2022-10-30 16:22:45 +0500

    ru.job4j.tracker.Tracker

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -61,8 +61,7 @@ public class Tracker {
         int index = indexOf(id);
         if (index != -1) {
             items[index] = null;
-            int length = size - index - 1;
-            System.arraycopy(items, index + 1, items, index, length);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
             rsl = true;
