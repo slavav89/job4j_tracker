4adf7082226f566783fcce9060f82880d87b7175
Вячеслав <slava_zen@mail.ru>
2022-11-20 19:03:08 +0500

    ru.job4j.strategy.Paint

--- src/main/java/ru/job4j/strategy/Square.java
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -1,10 +1,10 @@
 package ru.job4j.strategy;
 
-public class Square implements Shape{
+public class Square implements Shape {
     @Override
     public String draw() {
         String ln = System.lineSeparator();
-        return    " _____ " + ln
+        return " _____ " + ln
                 + "|     |" + ln
                 + "|_____|" + ln;
     }
