e8422a1fa999393f621a8a8ea42b6080dd405bcf
Вячеслав <slava_zen@mail.ru>
2022-12-25 19:12:06 +0500

    ru.job4j.enums.CarService

--- src/main/java/ru/job4j/enums/CarService.java
+++ src/main/java/ru/job4j/enums/CarService.java
@@ -8,16 +8,10 @@ public class CarService {
         Status volvo = Status.WAITING;
         Status lada = Status.ACCEPTED;
         Status bmw = Status.IN_WORK;
-//        System.out.println(toyota);
-//        System.out.println(volvo);
-//        System.out.println(lada);
-//        System.out.println(bmw);
 
         Order order = new Order(1, "Mercedes-Benz GLS", Status.IN_WORK);
         System.out.println("Заказ-наряд №" + order.getId() + " на автомобиль " + order.getModel()
                 + ", статус ремонта: " + order.getStatus().getInfo() + ", подробности: " + order.getStatus().getMessage());
-
-        //System.out.println(order);
         Status[] statuses = Status.values();
         for (Status s : statuses) {
             System.out.println("Название статуса: " + s.name() + ", Порядковый номер статуса: " + s.ordinal());
