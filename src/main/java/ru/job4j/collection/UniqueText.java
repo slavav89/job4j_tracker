0a14da5275b9918f4b1fde66af264e9d43db7d08
Вячеслав <slava_zen@mail.ru>
2022-12-30 18:21:03 +0500

    ru.job4j.collection.UniqueText

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -14,6 +14,7 @@ public class UniqueText {
         for (String word : text) {
             if (!check.contains(word)) {
                 rsl = false;
+                break;
             }
         }
         return rsl;
