84144b6d016e138750ec36406f9576a453f6a23a
Вячеслав <slava_zen@mail.ru>
2023-02-09 21:04:13 +0500

    ru.job4j.lambda.lazyLoading

---
+++ src/main/java/ru/job4j/lambda/lazyLoading.java
@@ -0,0 +1,18 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class lazyLoading {
+    public static void main(String[] args) {
+        String[] names = {
+                "Slava",
+                "Ivan"
+        };
+        Comparator<String> lengthCmp = (left, right) -> {
+            System.out.println("execute comparator");
+            return Integer.compare(left.length(), right.length());
+        };
+        Arrays.sort(names, lengthCmp);
+    }
+}
