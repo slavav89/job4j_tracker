32131bf85eaae988628cd7e1e3bf859139a00103
Вячеслав <slava_zen@mail.ru>
2023-02-13 17:04:32 +0500

    ru.job4j.function.FunctionCalculate

---
+++ src/test/java/ru/job4j/function/FunctionCalculateTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.function;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FunctionCalculateTest {
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        FunctionCalculate function = new FunctionCalculate();
+        List<Double> result = function.diapason(10, 13, x -> x - 1);
+        List<Double> expected = Arrays.asList(9D, 10D, 11D);
+        assertThat(result).containsAll(expected);
+    }
+
+    @Test
+    public void whenQuadraticFunctionThenQuadraticResult() {
+        FunctionCalculate function = new FunctionCalculate();
+        List<Double> result = function.diapason(1, 4, x -> x * x + 1);
+        List<Double> expected = Arrays.asList(2D, 5D, 10D);
+        assertThat(result).containsAll(expected);
+    }
+
+    @Test
+    public void whenIndicativeFunctionThenIndicativeResult() {
+        FunctionCalculate function = new FunctionCalculate();
+        List<Double> result = function.diapason(3, 5, x -> Math.pow(x, 3));
+        List<Double> expected = Arrays.asList(27D, 64D);
+        assertThat(result).containsAll(expected);
+    }
+}
