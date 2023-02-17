fe0a3e50cafe55ba888b6375d0c4f11e07ebff28
Вячеслав <slava_zen@mail.ru>
2023-02-17 20:10:23 +0500

    ru.job4j.function.FunctionalInterfaces

--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -9,7 +9,7 @@ import java.util.function.*;
 public class FunctionalInterfaces {
     public static void main(String[] args) {
         Map<Integer, String> map = new HashMap<>();
-        BiConsumer<Integer, String> biCon = (i, s) -> map.put(i, s);
+        BiConsumer<Integer, String> biCon = map::put;
         biCon.accept(1, "one");
         biCon.accept(2, "two");
         biCon.accept(3, "three");
@@ -18,7 +18,7 @@ public class FunctionalInterfaces {
         biCon.accept(6, "six");
         biCon.accept(7, "seven");
 
-        BiPredicate<Integer, String> biPredicate = (i, s) -> i % 2 == 0 || map.get(i).length() == 4;
+        BiPredicate<Integer, String> biPredicate = (i, s) -> i % 2 == 0 || s.length() == 4;
         for (Integer i : map.keySet()) {
             if (biPredicate.test(i, map.get(i))) {
                 System.out.println("key: " + i + " value: " + map.get(i));
@@ -28,8 +28,8 @@ public class FunctionalInterfaces {
         Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
         List<String> strings = sup.get();
 
-        Consumer<String> con = s -> System.out.println(s);
-        Function<String, String> func = (s) -> s.toUpperCase();
+        Consumer<String> con = System.out::println;
+        Function<String, String> func = String::toUpperCase;
         for (String s : strings) {
             con.accept(func.apply(s));
         }
