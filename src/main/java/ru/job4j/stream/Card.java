1a467c19c8f0783b9139493f7e89524176e2677d
Вячеслав <slava_zen@mail.ru>
2023-02-25 12:06:21 +0500

    ru.job4j.stream.Card

---
+++ src/main/java/ru/job4j/stream/Card.java
@@ -0,0 +1,28 @@
+package ru.job4j.stream;
+
+import java.util.stream.Stream;
+
+public class Card {
+    private Suit suit;
+    private Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{"
+                + "suit=" + suit
+                + ", value=" + value
+                + '}';
+    }
+
+    public static void main(String[] args) {
+        Stream.of(Suit.values())
+                .flatMap(suit1 -> Stream.of(Value.values())
+                        .map(value1 -> new Card(suit1, value1).toString()))
+                .forEach(System.out::println);
+    }
+}
